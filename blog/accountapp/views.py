5842acf0d32fde5891ae5974bfd27bf16005c03d
dove9441 <dotml386@naver.com>
2022-10-30 13:29:06 +0000

    djust djangop course 8 commit

--- blog/accountapp/views.py
+++ blog/accountapp/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse #추가해야 한다
 
 
 def hello_world(request):
-    return HttpResponse("hello world!")
+    #return HttpResponse("hello world!") #HttpResponse를 직접 리턴
+    return render(request, 'base.html')
+#처음 이렇게 만들고 root에서 setting.py에서 템플릿 경로 추가
+
+
+
+
+
+
 #여기서 만들고 최초 폴더의 urls.py에서 라우팅
 
 
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #여기서 템플릿의 경로를 추가
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
