7d4811b6ba654e149cbe08a5bb3a30441e175f87
dove9441 <dotml386@naver.com>
2022-11-04 13:21:05 +0000

    django course 14 commit

--- blog/accountapp/models.py
+++ blog/accountapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# 모덷 클래스를 작성하고 나서 python manage.py makemigrations을 해야 blog/migrations에 새로운 .py가 생긴다.
+# 이후 python manage.py migrate를 실행한다. 그러면 자동으로 db와 연동이 된다.
+# blog/migrations 안의 파일은 삭제하는 다른 방법이 있으니 임의로 삭제하지 말자
+
+
+
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
