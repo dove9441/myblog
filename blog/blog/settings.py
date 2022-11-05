a675c228719a1f1e42c6d52076a99b391392014b
dove9441 <dotml386@naver.com>
2022-11-05 10:55:40 +0000

    django course 23 commit

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp', #여기서 startapp으로 추가한 앱을 추가해야 한다. 마지막 ,도.
+    'bootstrap4', #부트스트랩 설치 후 적기
 ]
 
 MIDDLEWARE = [
@@ -132,4 +133,4 @@ STATICFILES_DIRS = [
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login') #로그아웃 시 윗줄의 LOGIN_REDIRECT_URL로 이동. 여기서 또 hello_world로 이동한다.
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login') #로그아웃 시 윗줄의 LOGIN_REDIRECT_URL로 이동. 여기서 또 hello_world로 이동한다. 결국 로그인, 로그아웃 시 모두 hello_world로 이동.
